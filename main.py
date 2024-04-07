4c515017fe8cb2c75db604527ffc43df98c44aac
Guberapandian <139293210+Guberapandian@users.noreply.github.com>
2024-04-07 11:30:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 #templates = Jinja2Templates(directory="/code")
-templates = Jinja2Templates(directory="/D:/Manoj/My Class/Devops/Kubernate/k8s project new file/k8_project_Manoj")
+templates = Jinja2Templates(directory="/C:\Users\Admin\Downloads\k8_project_Manoj")
 @app.get("/")
 def form_post(request: Request):
     return templates.TemplateResponse('form.html', context={'request': request})
