2b01c0821f09203b3c87d60c6549d39e3bb8e607
Guberapandian <139293210+Guberapandian@users.noreply.github.com>
2024-04-24 17:13:14 +0530

    manojcommit

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-#templates = Jinja2Templates(directory="/code")
-templates = Jinja2Templates(directory="/C:\Users\Admin\Downloads\k8_project_Manoj")
+templates = Jinja2Templates(directory="/code")
+#templates = Jinja2Templates(directory="/C:\Users\Admin\Downloads\k8_project_Manoj")
 @app.get("/")
 def form_post(request: Request):
     return templates.TemplateResponse('form.html', context={'request': request})
